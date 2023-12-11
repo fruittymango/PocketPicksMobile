1146e850b7b6deaef07d24c1924b33e11e3064b7
Isaac Mogano <kaymogano@gmail.com>
2023-12-11 16:25:48 +0200

    Updated pre build script

--- appcenter-post-clone.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Creating .env file"
-cat > ./.env <<EOL
-API_KEY=${API_KEY}
-EOL
---
+++ appcenter-pre-build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Get the API_KEY from the environment variables
+API_KEY=$API_KEY
+
+if [ -z "$API_KEY" ]; then
+  echo "API_KEY is not defined in the environment variables."
+  exit 1
+fi
+
+# Generate or modify .env file content
+echo "API_KEY=$API_KEY" > .env
+
+echo ".env file generated for App Center pre-build script."
